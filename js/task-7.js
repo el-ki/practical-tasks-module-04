7df40b456d9b0db51b16c2d0d2f0eb18b852bfd2
Elena Kichinskaia <nitress@gmail.com>
2021-02-05 00:34:27 +0200

    7 tasks completed

---
+++ js/task-7.js
@@ -0,0 +1,17 @@
+
+
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки
+
+function composeMessage(position) {
+    return`Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`
+}
+const messages = orders.map((order, index) => {
+    return composeMessage.call(order, index + 1);  
+})
+ console.log(messages); // ["Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.", "Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.", "Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди."]
