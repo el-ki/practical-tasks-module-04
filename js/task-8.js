730cfe2bf5eeaae9804d5b398524bcf0ce0de4ba
Elena Kichinskaia <nitress@gmail.com>
2021-02-09 19:58:18 +0200

    10 tasks completed

--- js/task-8.js
+++ js/task-8.js
@@ -15,14 +15,18 @@
 //   composeMessage.call(order, index + 1)
 // );
 
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
 
-// Тесты
-// Объявлена переменная orders.
-// Значение переменной orders это исходный массив объектов-заказов.
-// Объявлена функция composeMessage(position).
-// У функции composeMessage объявлен один параметр position.
-// В теле функции composeMessage используется this.
-// Объявлена переменная messages.
-// Переменной messages с помощью метода map присваивают значение - массив, создаваемый вызовом функции composeMessage.
-// Значение this при вызове функции переопределяется с помощью метода apply.
-// Значение переменной messages это массив ['Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.', 'Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.', 'Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди.'].
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = orders.map((order, index) =>
+  composeMessage.apply(order, [index + 1])
+);
+ console.log(messages); // ["Готовим Burger для solomon@topmail.ua. Ваш заказ 1-й в очереди.", "Готовим Pizza для artemis@coldmail.net. Ваш заказ 2-й в очереди.", "Готовим Taco для jacob@mail.com. Ваш заказ 3-й в очереди."]
